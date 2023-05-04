c8468c3f3d9cc1bde0c70c834869864aaecf029c
Tanvir Rahman <mdtanvirt@gmail.com>
2023-05-04 17:40:00 +0600

    Update dashboard

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 import pydeck as pdk
 import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+import altair as alt
+from ipyvizzu import Chart, Data, Config, Style
 from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide")
@@ -18,6 +22,10 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 data_url= "https://gist.githubusercontent.com/florianeichin/cfa1705e12ebd75ff4c321427126ccee/raw/c86301a0e5d0c1757d325424b8deec04cc5c5ca9/flights_all_cleaned.csv"
 data = pd.read_csv(data_url)
+# df for airlines frequency 
+df_airlines_frequency_count = data.groupby(['AIRLINE']).agg(
+    count = pd.NamedAgg(column='AIRLINE', aggfunc='count')
+)
 
 with st.sidebar:
     nav_menu = option_menu("Main Menu", ["Dashboard", "Map Analyzer", 'Raw Data'], 
@@ -25,6 +33,23 @@ with st.sidebar:
 
 if nav_menu == "Dashboard":
     st.header("Dashboard")
+
+    col_barChart, col_scat_chart = st.columns(2)
+
+    with col_barChart:
+        st.write('Number of flights operate by Airline')
+        st.bar_chart(df_airlines_frequency_count)
+    
+    with col_scat_chart:
+        st.write('Elapsed Time vs Distance')
+        chart = alt.Chart(data).mark_circle(size=60).encode(
+            x='DISTANCE',
+            y='ELAPSED_TIME',
+            color='AIRLINE',
+            tooltip=['AIRLINE', 'ORIGIN_AIRPORT', 'ELAPSED_TIME', 'DISTANCE']
+        ).interactive()
+        st.altair_chart(chart, theme="streamlit", use_container_width=True)
+        #st.line_chart(['DEPARTURE_DELAY', 'AIRLINE'])
 
 elif nav_menu == "Map Analyzer":
     st.header("Data exploration with map")
