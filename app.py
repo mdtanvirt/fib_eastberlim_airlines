f8ca43c064f259e0274f821b029a606988ac9006
Tanvir Rahman <mdtanvirt@gmail.com>
2023-05-04 13:44:42 +0600

    Map integration

--- app.py
+++ app.py
@@ -29,71 +29,36 @@ if nav_menu == "Dashboard":
 elif nav_menu == "Query Analyzer":
     st.header("Data exploration with map")
 
-    '''
-    ###############
-     # Use pandas to prepare data for tooltip
-    #data["ORIGIN_AIRPORT"] = data["ORIGIN_AIRPORT_POS"].apply(lambda f: f["ORIGIN_AIRPORT_LON", "ORIGIN_AIRPORT_LAT"])
-    #data["DESTINATION_AIRPORT"] = data["DESTINATION_AIRPORT_POS"].apply(lambda t: t["DESTINATION_AIRPORT_LON", "DESTINATION_AIRPORT_LAT"])
-
-    # Define a layer to display on a map
-    GREEN_RGB = [0, 255, 0, 40]
-    RED_RGB = [240, 100, 0, 40]
-
-    layer = pdk.Layer(
-        "GreatCircleLayer",
-        data,
-        pickable=True,
-        get_stroke_width=12,
-        get_source_position=["ORIGIN_AIRPORT_LON", "ORIGIN_AIRPORT_LAT"],
-        get_target_position=["DESTINATION_AIRPORT_LON", "DESTINATION_AIRPORT_LAT"],
-        get_source_color=RED_RGB,
-        get_target_color=GREEN_RGB,
-        auto_highlight=True,
-
-    )
-
-    view_state = pdk.ViewState(bearing=45, pitch=50, zoom=8,)
-
-    TOOLTIP_TEXT = {"html": "{S000} jobs <br /> Home of commuter in red; work location in green"}
-    r = pdk.Deck(layer, initial_view_state=view_state, tooltip=TOOLTIP_TEXT)
-    r.to_html("layer.html")'''
-
-    ###########################
-
     chart_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
+    GREEN_RGB = [0, 255, 0, 0]
+    RED_RGB = [250, 100, 0, 40]
+
+
     st.pydeck_chart(pdk.Deck(
         map_style=None,
         initial_view_state=pdk.ViewState(
-            latitude=37.76,
-            longitude=-122.4,
-            zoom=11,
-            pitch=50,
+            latitude=38.5260,
+            longitude=-115.766,
+            zoom=3,
+            pitch=2,
         ),
         layers=[
             pdk.Layer(
-            'HexagonLayer',
-            data=chart_data,
-            get_position='[lon, lat]',
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
+            "GreatCircleLayer",
+            data=data,
             pickable=True,
-            extruded=True,
-            ),
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=chart_data,
-                get_position='[lon, lat]',
-                get_color='[200, 30, 0, 160]',
-                get_radius=200,
+            get_stroke_width=20,
+            get_source_position=["ORIGIN_AIRPORT_LON", "ORIGIN_AIRPORT_LAT"],
+            get_target_position=["DESTINATION_AIRPORT_LON", "DESTINATION_AIRPORT_LAT"],
+            get_source_color=RED_RGB,
+            get_target_color=GREEN_RGB,
+            auto_highlight=True,
             ),
         ],
     ))
-
-    ##########################
 
    
 
